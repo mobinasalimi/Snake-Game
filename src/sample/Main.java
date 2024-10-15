7142c0e44a1a61649c0f05d2acd376db0a093196
Mobina Salimipanah <79265118+mobinasalimi@users.noreply.github.com>
2024-10-15 17:48:49 +0330

    src added

---
+++ src/sample/Main.java
@@ -0,0 +1,231 @@
+package sample;
+
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main extends Application {
+
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = WIDTH;
+    private static final int ROWS = 20;
+    private static final int COLUMNS = ROWS;
+    private static final int SQUARE_SIZE = WIDTH / ROWS;
+    private static final String[] FRUITS_IMAGES = new String[]{"/img/Apple.png"};
+
+    private static final int RIGHT = 0;
+    private static final int LEFT = 1;
+    private static final int UP = 2;
+    private static final int DOWN = 3;
+
+    private GraphicsContext gc;
+    private List<Point> snakeBody = new ArrayList();
+    private Point snakeHead;
+    private Image fruitImage;
+    private int fruitX;
+    private int fruitY;
+    private boolean gameOver;
+    private int currentDirection;
+    private int score = 0;
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Snake");
+        Group root = new Group();
+        Canvas canvas = new Canvas(WIDTH, HEIGHT);
+        root.getChildren().add(canvas);
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        gc = canvas.getGraphicsContext2D();
+
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                KeyCode code = event.getCode();
+                if (code == KeyCode.RIGHT || code == KeyCode.D) {
+                    if (currentDirection != LEFT) {
+                        currentDirection = RIGHT;
+                    }
+                } else if (code == KeyCode.LEFT || code == KeyCode.A) {
+                    if (currentDirection != RIGHT) {
+                        currentDirection = LEFT;
+                    }
+                } else if (code == KeyCode.UP || code == KeyCode.W) {
+                    if (currentDirection != DOWN) {
+                        currentDirection = UP;
+                    }
+                } else if (code == KeyCode.DOWN || code == KeyCode.S) {
+                    if (currentDirection != UP) {
+                        currentDirection = DOWN;
+                    }
+                }
+            }
+        });
+        for (int i = 0; i < 3; i++) {
+            snakeBody.add(new Point(5, ROWS / 2));
+        }
+        snakeHead = snakeBody.get(0);
+        generateFruit();
+        Timeline timeline = new Timeline(new KeyFrame(Duration.millis(130), e -> run(gc)));
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
+    }
+
+
+    private void run(GraphicsContext gc) {
+        if (gameOver) {
+            gc.setFill(Color.RED);
+            gc.setFont(new Font("Digital-7", 70));
+            gc.fillText("Game Over!", WIDTH / 3.5, HEIGHT / 2);
+            return;
+        }
+        paintBackground(gc);
+        drawFruit(gc);
+        drawSnake(gc);
+        drawScore();
+
+
+        for (int i = snakeBody.size() - 1; i >= 1; i--) {
+            snakeBody.get(i).x = snakeBody.get(i - 1).x;
+            snakeBody.get(i).y = snakeBody.get(i - 1).y;
+
+        }
+        switch (currentDirection) {
+            case RIGHT:
+                moveRight();
+                break;
+            case LEFT:
+                moveLeft();
+                break;
+            case UP:
+                moveUp();
+                break;
+            case DOWN:
+                moveDown();
+                break;
+        }
+
+        gameOver();
+        eatFruits();
+    }
+
+    private void paintBackground(GraphicsContext gc) {
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLUMNS; j++) {
+                if ((i + j) % 2 == 0) {
+                    gc.setFill(Color.web("AAD751"));
+                } else {
+                    gc.setFill(Color.web("A2D149"));
+                }
+                gc.fillRect(i * SQUARE_SIZE, j * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE);
+
+            }
+
+        }
+
+    }
+
+    private void generateFruit() {
+        start:
+        while (true) {
+            fruitX = (int) (Math.random() * ROWS);
+            fruitY = (int) (Math.random() * COLUMNS);
+
+            for (Point snake : snakeBody) {
+                if (snake.getX() == fruitX && snake.getY() == fruitY) {
+                    continue start;
+                }
+            }
+
+            fruitImage = new Image(FRUITS_IMAGES[(int) (Math.random() * FRUITS_IMAGES.length)]);
+            break;
+
+        }
+
+    }
+
+    private void drawFruit(GraphicsContext gc) {
+        gc.drawImage(fruitImage, fruitX * SQUARE_SIZE, fruitY * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE);
+    }
+
+    private void drawSnake(GraphicsContext gc) {
+        gc.setFill(Color.web("4674E9"));
+        gc.fillRoundRect(snakeHead.getX() * SQUARE_SIZE, snakeHead.getY() * SQUARE_SIZE, SQUARE_SIZE - 1, SQUARE_SIZE - 1, 35, 35);
+        for (int i = 1; i < snakeBody.size(); i++) {
+            gc.fillRoundRect(snakeBody.get(i).getX() * SQUARE_SIZE, snakeBody.get(i).getY() * SQUARE_SIZE, SQUARE_SIZE - 1,
+                    SQUARE_SIZE - 1, 20, 20);
+
+
+        }
+
+    }
+
+    public void moveRight() {
+        snakeHead.x++;
+    }
+
+    public void moveLeft() {
+        snakeHead.x--;
+    }
+
+    public void moveUp() {
+        snakeHead.y--;
+    }
+
+    public void moveDown() {
+        snakeHead.y++;
+    }
+
+    public void gameOver() {
+        if (snakeHead.x < 0 || snakeHead.y < 0 || snakeHead.x * SQUARE_SIZE >= WIDTH || snakeHead.y * SQUARE_SIZE >= HEIGHT) {
+            gameOver = true;
+        }
+
+        for (int i = 1; i < snakeBody.size(); i++) {
+            if (snakeHead.x == snakeBody.get(i).getX() && snakeHead.getY() == snakeBody.get(i).getY()) {
+                gameOver = true;
+                break;
+            }
+
+        }
+    }
+
+    private void eatFruits() {
+        if (snakeHead.getX() == fruitX && snakeHead.getY() == fruitY) {
+            snakeBody.add(new Point(-1, -1));
+            generateFruit();
+            score += 2;
+        }
+    }
+
+    private void drawScore() {
+        gc. setFill(Color.WHITE);
+        gc.setFont(new Font( "Digital - 7" , 35));
+        gc.fillText("Score: "+score , 10 , 35);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
